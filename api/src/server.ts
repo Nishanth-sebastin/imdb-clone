0e1679385c522cb18f28e2e948aedf24e5fff183
Nishanth-sebastin <Nishanthsebastinj24cseb@gmail.com>
2025-03-16 00:48:06 +0530

    Remove unnecessary CORS credentials and methods configuration in server.ts for cleaner code and improved clarity.

--- api/src/server.ts
+++ api/src/server.ts
@@ -20,11 +20,9 @@ const PORT = process.env.PORT || 8085;
 app.use(
   cors({
     origin: process.env.VITE_WEB_URL,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   })
 );
-console.log(process.env.VITE_WEB_URL)
+
 app.use(express.json());
 app.use(cookieParser());
 
